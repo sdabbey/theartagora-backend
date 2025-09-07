b722747c9c8cca226dd308fbb2a7d60691c176d3
Samuel Desmond Abbey Darko <dehydrus223@gmail.com>
2025-09-07 21:23:58 +0000

    update

--- backend/settings.py
+++ backend/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["127.0.0.1", "localhost", "theartagora-mini.vercel.app"]
+ALLOWED_HOSTS = ["127.0.0.1", "localhost", "theartagora-mini.vercel.app", "theartagora-backend.up.railway.app"]
 CSRF_TRUSTED_ORIGINS = ["http://localhost:8000", "https://theartagora-mini.vercel.app", "https://theartagora-backend.up.railway.app"]
 
 
